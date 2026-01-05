ee252c0424b17f278937d8e7793a23e596975ae0
Midhun-u <midhunvkm689@gmail.com>
2026-01-05 19:38:45 +0530

    Completed the function

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
+/**
+    @param {string} password - The password which is for checking if the password is strong
+    @param {number} minLength - The minimum length of the password should be
+    @returns {{level: "weak" | "medium" | "strong", score: number}}
+ */
+
 // Function for checking password is strong
+const showPasswordStrength = (password, minLength) => {
+
+    const hasLower = /[a-z]/.test(password) // For checking if password contains lower characters 
+    const hasUpper = /[A-Z]/.test(password) // For checking if password contains upper characters
+    const hasNumber = /[0-9]/.test(password) // For checking if password contains numbers
+    const hasSpecialCharacters = /[^A-Za-z0-9]/.test(password) // For checking if password contains special characters
+
+    if (password.length < minLength) {
+        return { level: "weak", score: 100 / 4 }
+    }
+
+    if(
+        (password.length >= minLength + minLength) &&
+        hasLower &&
+        hasNumber &&
+        hasUpper &&
+        hasSpecialCharacters
+    ){
+        return {level: "strong", score: 100}
+    }
+
+    if(
+        password.length >= minLength &&
+        hasLower &&
+        hasNumber &&
+        hasUpper &&
+        hasSpecialCharacters
+    ){
+        return {level: "strong", score: 75}
+    }
+
+    if (
+        password.length >= minLength &&
+        hasLower &&
+        hasUpper &&
+        hasNumber
+    ) {
+        return { level: "strong", score: Math.round(100 / 2) }
+    }
+
+    if (
+        (password.length >= minLength) &&
+        hasLower ||
+        hasUpper ||
+        hasNumber ||
+        hasSpecialCharacters
+    ) {
+
+        return { level: "medium", score: Math.round(100 / 3) }
+
+    }
+}
